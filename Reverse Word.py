53bbd19cf317b0db2db4ecb4c938e8b73d243ce2
A.B.M. Rokon-Uz-Zaman Roman <mdroman266@gmail.com>
2022-03-02 22:59:39 +0600

    Update Reverse Word.py

--- Reverse Word.py
+++ Reverse Word.py
@@ -1,16 +1,11 @@
 #How to reverse every word of a sentense ( string)
 
 rev=''
-
 sentense="Banglasesh is nice country"
 
 sentense=sentense.split()
 sentense.reverse()
 
-
-
 for element in sentense:
     rev=rev+" "+element
 print(rev)    
-
-
