3abb3efb3c882d06c617d374b976c040e878f95f
A.B.M. Rokon-Uz-Zaman Roman <mdroman266@gmail.com>
2022-05-14 12:37:22 +0600

    Update find extra char in string.py

--- find extra char in string.py
+++ find extra char in string.py
@@ -1,1 +1,15 @@
 #Finding extra char in string 
+
+string1=input()
+string2=input()
+
+for element in string2:
+    #conditon1 : abcd , abcde
+    if element not in string1: 
+       print(element) 
+    
+    #condtion 2 : abcd , abcdd
+    elif element in string1 and string2.count(element)>1 :
+      print(element)
+      break  
+       
